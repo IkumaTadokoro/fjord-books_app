10352a35cafa10eff760efcad462a20d86227064
IkumaTadokoro <tadokorodev@gmail.com>
2021-01-01 22:34:37 +0900

    Deviseのomniauthableモジュールを有効に
    クライアントアプリが要求するスコープは`{read:user}`とする

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable
+
+  validates :uid, uniqueness: { scope: :provider }
+
+  def self.find_or_create_from_auth_hash!(auth)
+    find_or_create_by!(provider: auth.provider, uid: auth.uid) do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token
+    end
+  end
 end
