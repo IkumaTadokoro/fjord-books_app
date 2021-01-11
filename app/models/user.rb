90f4277cadca16ebf7ea8b4ab05e8057c05d0c3a
IkumaTadokoro <tadokorodev@gmail.com>
2021-01-11 16:52:07 +0900

    Relationshipモデルとアソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
+  has_many :relationships_as_follower, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy
+  has_many :followings, through: :relationships_as_follower
+
+  has_many :relationships_as_following, class_name: 'Relationship', foreign_key: :following_id, dependent: :destroy
+  has_many :followers, through: :relationships_as_following
+
   validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
   def self.from_omniauth(auth)
@@ -15,5 +21,21 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
     end
+  end
+
+  def follow(user)
+    relationships_as_follower.create!(following_id: user.id)
+  end
+
+  def unfollow(user)
+    relationships_as_follower.find_by(following_id: user.id).destroy!
+  end
+
+  def following?(user)
+    relationships_as_follower.where(following_id: user.id).exists?
+  end
+
+  def followed?(user)
+    relationships_as_following.where(follower_id: user.id).exists?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_27_052521) do
+ActiveRecord::Schema.define(version: 2021_01_11_055906) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,16 @@ ActiveRecord::Schema.define(version: 2020_12_27_052521) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
     t.string "picture"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "following_id", null: false
+    t.integer "follower_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["following_id", "follower_id"], name: "index_relationships_on_following_id_and_follower_id", unique: true
+    t.index ["following_id"], name: "index_relationships_on_following_id"
   end
 
   create_table "users", force: :cascade do |t|
