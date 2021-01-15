66a4378ac491588cfda74fcd540ed29d93988b3c
IkumaTadokoro <tadokorodev@gmail.com>
2021-01-15 21:13:20 +0900

    フォロー一覧・フォロワー一覧ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   resources :books
   resources :users, only: %i(index show) do
     resource :relationships, only: %i[create destroy]
+    scope module: :users do
+      resources :followings, only: %i[index]
+      resources :followers, only: %i[index]
+    end
   end
 end
