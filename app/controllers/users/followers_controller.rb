2b66cebe70bdcb7a75c23cbb653c6dd287779bd0
IkumaTadokoro <tadokorodev@gmail.com>
2021-01-16 11:16:42 +0900

    ActiveStorage起因のN+1問題を解消

--- app/controllers/users/followers_controller.rb
+++ app/controllers/users/followers_controller.rb
@@ -3,6 +3,6 @@
 class Users::FollowersController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @followers = @user.followers.order(:id).page(params[:page])
+    @followers = @user.followers.with_attached_avatar.order(:id).page(params[:page])
   end
 end
