5a5879ad3ba198ab86d03cb975d061ef79e4e3a3
IkumaTadokoro <tadokorodev@gmail.com>
2021-02-03 20:46:09 +0900

    Userモデルのモデルテストを追加

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :user do
-    sequence(:name) { |i| "name#{i}" }
-    sequence(:email) { |i| "sample#{i}@example.com" }
-    sequence(:password) { |i| "password#{i}" }
+  factory :alice, class: User do
+    name { 'Alice' }
+    email { 'alice@example.com' }
+    password { 'alice_password' }
+  end
+
+  factory :bob, class: User do
+    provider { 'github' }
+    name { 'Bob' }
+    email { 'bob@example.com' }
+    password { 'bob_password' }
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#follow / #unfollow' do
+    alice = create(:alice)
+    bob = create(:bob)
+
+    assert_not alice.following?(bob)
+    assert_not bob.followed_by?(alice)
+    alice.follow(bob)
+    assert alice.following?(bob)
+    alice.unfollow(bob)
+    assert_not alice.following?(bob)
+  end
+
+  test '#name_or_email' do
+    alice = FactoryBot.create(:alice)
+    assert 'Alice', alice.name_or_email
+    alice.name = ''
+    assert 'alice.example.com', alice.name_or_email
+  end
 end
