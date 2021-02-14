1bed5869dfd24c941c770375af1b21b698dee041
IkumaTadokoro <tadokorodev@gmail.com>
2021-02-14 19:20:45 +0900

    setup中ではなく、各テストケース内で必要なデータを定義するよう変更

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,18 +3,16 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  setup do
-    @owner = create(:user)
-    @not_owner_user = create(:user)
-    @report = create(:report, user: @owner)
-  end
-
   test '#editable?' do
-    assert @report.editable?(@owner)
-    assert_not @report.editable?(@not_owner_user)
+    owner = create(:user)
+    not_owner_user = create(:user)
+    report = create(:report, user: owner)
+    assert report.editable?(owner)
+    assert_not report.editable?(not_owner_user)
   end
 
   test '#created_on?' do
-    assert_equal(Date.current, @report.created_on)
+    report = create(:report)
+    assert_equal(Date.current, report.created_on)
   end
 end
